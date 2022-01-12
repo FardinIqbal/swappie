353080b8317f26426e1cfbdb2a30233a80aa1ab3
FardinIqbal <52869603+FardinIqbal@users.noreply.github.com>
2022-01-12 11:04:56 -0500

    Users can logout

--- app/src/main/java/com/example/swappapp/ProfileActivity.java
+++ app/src/main/java/com/example/swappapp/ProfileActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,6 +15,9 @@ import com.google.firebase.auth.FirebaseUser;
 public class ProfileActivity extends AppCompatActivity {
     // Firebase auth
     FirebaseAuth firebaseAuth;
+
+    // Views
+    private TextView mProfileTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,9 @@ public class ProfileActivity extends AppCompatActivity {
 
         // Initialize firebaseAuth
         firebaseAuth = FirebaseAuth.getInstance();
+
+        // Initialize Views
+        mProfileTv = findViewById(R.id.profileTv);
     }
 
     private void checkUserStatus() {
@@ -32,7 +41,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         if(user != null) {
             // User is signed in stay here
-
+            mProfileTv.setText(user.getEmail());
         }
         else {
             // User not signed in, go to main activity
@@ -45,5 +54,26 @@ public class ProfileActivity extends AppCompatActivity {
         // check on start of app
         checkUserStatus();
         super.onStart();
+    }
+
+    // Inflate options menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // Handle menu item clicks
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // get item id
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
